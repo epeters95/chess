4cb350d29e912268a2334cd6fcf104c627832938
Erik <ewpeters@asu.edu>
2024-04-14 23:53:49 -0700

    add status string to game model

--- chess-server/db/schema.rb
+++ chess-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_13_084821) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_15_030345) do
   create_table "boards", force: :cascade do |t|
     t.string "turn"
     t.string "status_str", default: ""
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_13_084821) do
     t.string "black_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "moves", force: :cascade do |t|
