a883f1074641f0cdc1f21f24d15b563519360067
Erik <ewpeters@asu.edu>
2024-05-26 22:52:21 -0700

    fix/move player tests to requests

--- chess-server/spec/models/player_spec.rb
+++ chess-server/spec/models/player_spec.rb
@@ -3,33 +3,8 @@ require 'rails_helper'
 RSpec.describe Player, type: :model do
   describe 'creating a game' do
 
-
-    params = {
-      white_name: "Whitey",
-      black_name: "Blacky"
-    }
-
-    it 'creates a player by name if none exists' do
-      g = Game.create(params)
-
-      found_white = Player.find_by_name(params[:white_name])
-      found_black = Player.find_by_name(params[:black_name])
-      expect(found_white).to_not eq(nil)
-      expect(found_black).to_not eq(nil)
-    end
-
-    it 'attaches player reference if name exists' do
-
-      new_player = Player.create(name: params[:white_name])
-
-      g = Game.create(params)
-
-      found_white = Player.find_by_name(params[:white_name])
-      expect(found_white).to_not eq(nil)
-
-      expect(g.white_id).to eq(new_player.id)
-
-    end
+    # Ignore or replace with alternate tests
+    # Player creation logic does not exist on game model
 
   end
 end
--- chess-server/spec/requests/players_spec.rb
+++ chess-server/spec/requests/players_spec.rb
@@ -26,4 +26,38 @@ RSpec.describe "Players", type: :request do
       end
     end
   end
+
+  # Creating games
+  describe 'POST /api/games' do
+
+    game_params = {
+      game: {
+        white_name: "Jimmy",
+        black_name: "Bob"
+      }
+    }
+    white_player = Player.create(game_params[:game][:white_name])
+
+    it 'creates a player by name if none exists' do
+      post '/api/games', params: game_params
+
+      expect(response.status).to eql(201)
+
+      found_black = Player.find_by_name(game_params[:game][:black_name])
+      expect(found_black).to_not eq(nil)
+    end
+
+    it 'attaches player reference if name exists' do
+
+      post '/api/games', params: game_params
+
+      expect(response.status).to eql(201)
+
+      found_white = Player.find_by_name(game_params[:game][:white_name])
+      expect(found_white).to_not eq(nil)
+
+      expect(g.white_id).to eq(white_player.id)
+
+    end
+
 end
