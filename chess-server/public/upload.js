94ec1c5db572122f72fadc8afef22ea3bd573269
Erik <ewpeters@asu.edu>
2024-06-06 23:43:45 -0700

    Upload status shown in UI

--- chess-server/public/upload.js
+++ chess-server/public/upload.js
@@ -1,14 +1,13 @@
 const pgnForm = document.getElementById("upload-pgn-form");
-
 const pgnSubmit = document.getElementById("upload-pgn-submit");
+const uploadStatusSpan = document.getElementById("upload-status-span");
 
 pgnSubmit.addEventListener("click", function(event) {
 
   let files = pgnForm.children[1].files;
   if (files.length > 0) {
     fetchFromApi("/api/boards/", "POST", {"pgn_text": files[0]}, function() {
-      console.log("File uploaded successfully!");
-
+      uploadStatusSpan.innerHTML("Upload Successful!");
     })
   }
 
