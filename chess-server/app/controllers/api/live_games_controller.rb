2dcd6837a570fa89f64288986cbb0d65a19a0fd6
Erik <ewpeters@asu.edu>
2024-06-12 01:49:20 -0700

    remove blank name fix - breaks live game creation

--- chess-server/app/controllers/api/live_games_controller.rb
+++ chess-server/app/controllers/api/live_games_controller.rb
@@ -47,8 +47,8 @@ class Api::LiveGamesController < ApplicationController
     p_team = params[:player_team]
     access_code = params[:access_code]
 
-    whitename = ( p_team == "white" ? p_name : "No name" )
-    blackname = ( p_team == "black" ? p_name : "No name" )
+    whitename = ( p_team == "white" ? p_name : "" )
+    blackname = ( p_team == "black" ? p_name : "" )
 
     @livegame = LiveGame.find_by(access_code: access_code)
     if @livegame.nil?
