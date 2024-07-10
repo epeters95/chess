2496778c75e37fb80af8119cae2bde236bc0dac7
Erik <ewpeters@asu.edu>
2024-07-09 23:59:48 -0700

    update specs

--- chess-server/spec/requests/api/boards_spec.rb
+++ chess-server/spec/requests/api/boards_spec.rb
@@ -1,7 +1,18 @@
-require 'rails_helper'
+require 'swagger_helper'
 
-RSpec.describe "Api::Boards", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Boards API', type: :request do
+  path '/api/boards/{id}' do
+
+    get('Show board by id') do
+      produces 'application/json'
+
+      parameter name: :id, in: :path, type: :string
+
+      response(200, 'successful') do
+        let(:id) { '1' }
+        run_test!
+      end
+    end
+
   end
 end
