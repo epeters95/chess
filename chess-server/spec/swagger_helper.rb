d872103415541dde43021d0542c12d2375b8f304
Erik <ewpeters@asu.edu>
2024-11-17 20:05:30 -0700

    fix issues with swagger rendering on server side /api-docs

--- chess-server/spec/swagger_helper.rb
+++ chess-server/spec/swagger_helper.rb
@@ -39,5 +39,5 @@ RSpec.configure do |config|
   # The openapi_specs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.openapi_format = :yaml
+  config.openapi_format = :json
 end
