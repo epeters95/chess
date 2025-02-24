92042fad689346f35dca714e003012fc6eef4d58
Erik <ewpeters@asu.edu>
2025-02-23 17:42:02 -0700

    add wins, losses, draws to players board

--- js/get_players.js
+++ js/get_players.js
@@ -4,7 +4,12 @@ let spinner = showSpinner("spinner-div");
 
 fetchFromApi('/api/players', 'GET', null, function(json) {
   let tableHeader = document.createElement('tr')
-  tableHeader.innerHTML = "<th>Name</th><th># Games</th><th>Completed</th>";
+  tableHeader.innerHTML = "<th>Name</th>" +
+                          "<th># Games</th>" +
+                          "<th>Completed</th>" +
+                          "<th>Wins</th>" +
+                          "<th>Losses</th>" +
+                          "<th>Draws</th>";
   playersTable.appendChild(tableHeader);
 
   // Currently, this client is using the initial method of linking users games
@@ -23,10 +28,25 @@ fetchFromApi('/api/players', 'GET', null, function(json) {
     tableCells += "</td><td>";
 
     if (player.completed_games > 0) {
-      link += '&status=completed'
-      tableCells += "<a href='" + link + "'>" + player.completed_games + "</a>"
+      tableCells += "<a href='" + link + "&status=completed" + "'>" + player.completed_games + "</a>"
+    }
+    tableCells += "</td><td>";
+
+    if (player.wins > 0) {
+      tableCells += "<a href='" + link + "&wins=" + player.id + "'>" + player.completed_games + "</a>"
+    }
+    tableCells += "</td><td>";
+
+    if (player.losses > 0) {
+      tableCells += "<a href='" + link + "&losses=" + player.id + "'>" + player.completed_games + "</a>"
+    }
+    tableCells += "</td><td>";
+
+    if (player.draws > 0) {
+      tableCells += "<a href='" + link + "&draws=" + player.id + "'>" + player.completed_games + "</a>"
     }
     tableCells += "</td>";
+
     tableRow.innerHTML = tableCells;
     playersTable.appendChild(tableRow)
   })
