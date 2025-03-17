b79d69fc070a9cf38a0fc4c3e1cab2eb0b122813
Erik <ewpeters@asu.edu>
2025-03-16 23:51:21 -0700

    switch to datatable for player scoreboard

--- js/get_players.js
+++ js/get_players.js
@@ -1,54 +1,73 @@
-const playersTable = document.getElementById("players-table");
+$(document).ready(function() {
 
-let spinner = showSpinner("spinner-div");
+let table = new DataTable('#players-table', {
+    ajax: {
+      "url": baseUrl + '/api/players',
+      "type": "GET",
+      "dataSrc": "players"
+    },
+    columns: [
+      { data: 'name' },
+      { data: 'games' },
+      { data: 'completed_games' },
+      { data: 'wins' },
+      { data: 'losses' },
+      { data: 'draws' }
+    ]
+});
 
-fetchFromApi('/api/players', 'GET', null, function(json) {
-  let tableHeader = document.createElement('tr')
-  tableHeader.innerHTML = "<th>Name</th>" +
-                          "<th># Games</th>" +
-                          "<th>Completed</th>" +
-                          "<th>Wins</th>" +
-                          "<th>Losses</th>" +
-                          "<th>Draws</th>";
-  playersTable.appendChild(tableHeader);
-
-  // Currently, this client is using the initial method of linking users games
-  // which is purely by unique name usage.
-  // To support future features linking players by id, this search endpoint will
-  // also support fetching games by white_id and black_id (not yet simply "id"...)
-  // in addition to "status" and quick ability to add more in the future
-  json["players"].forEach(function(player) {
-    let tableRow = document.createElement('tr');
-    let link = 'games_index.html?name=' + player.name;
-
-    let tableCells = "<td>" + player.name + "</td><td>";
-    if (player.games > 0) {
-      tableCells += player.games;
-    }
-    tableCells += "</td><td>";
-
-    if (player.completed_games > 0) {
-      tableCells += "<a href='" + link + "&status=completed" + "'>" + player.completed_games + "</a>"
-    }
-    tableCells += "</td><td>";
-
-    if (player.wins > 0) {
-      tableCells += "<a href='" + link + "&wins=" + player.id + "'>" + player.wins + "</a>"
-    }
-    tableCells += "</td><td>";
-
-    if (player.losses > 0) {
-      tableCells += "<a href='" + link + "&losses=" + player.id + "'>" + player.losses + "</a>"
-    }
-    tableCells += "</td><td>";
-
-    if (player.draws > 0) {
-      tableCells += "<a href='" + link + "&draws=" + player.id + "'>" + player.draws + "</a>"
-    }
-    tableCells += "</td>";
-
-    tableRow.innerHTML = tableCells;
-    playersTable.appendChild(tableRow)
-  })
-  spinner.hide();
 })
+// const playersTable = document.getElementById("players-table");
+
+// let spinner = showSpinner("spinner-div");
+
+// fetchFromApi('/api/players', 'GET', null, function(json) {
+//   let tableHeader = document.createElement('tr')
+//   tableHeader.innerHTML = "<th>Name</th>" +
+//                           "<th># Games</th>" +
+//                           "<th>Completed</th>" +
+//                           "<th>Wins</th>" +
+//                           "<th>Losses</th>" +
+//                           "<th>Draws</th>";
+//   playersTable.appendChild(tableHeader);
+
+//   // Currently, this client is using the initial method of linking users games
+//   // which is purely by unique name usage.
+//   // To support future features linking players by id, this search endpoint will
+//   // also support fetching games by white_id and black_id (not yet simply "id"...)
+//   // in addition to "status" and quick ability to add more in the future
+//   json["players"].forEach(function(player) {
+//     let tableRow = document.createElement('tr');
+//     let link = 'games_index.html?name=' + player.name;
+
+//     let tableCells = "<td>" + player.name + "</td><td>";
+//     if (player.games > 0) {
+//       tableCells += player.games;
+//     }
+//     tableCells += "</td><td>";
+
+//     if (player.completed_games > 0) {
+//       tableCells += "<a href='" + link + "&status=completed" + "'>" + player.completed_games + "</a>"
+//     }
+//     tableCells += "</td><td>";
+
+//     if (player.wins > 0) {
+//       tableCells += "<a href='" + link + "&wins=" + player.id + "'>" + player.wins + "</a>"
+//     }
+//     tableCells += "</td><td>";
+
+//     if (player.losses > 0) {
+//       tableCells += "<a href='" + link + "&losses=" + player.id + "'>" + player.losses + "</a>"
+//     }
+//     tableCells += "</td><td>";
+
+//     if (player.draws > 0) {
+//       tableCells += "<a href='" + link + "&draws=" + player.id + "'>" + player.draws + "</a>"
+//     }
+//     tableCells += "</td>";
+
+//     tableRow.innerHTML = tableCells;
+//     playersTable.appendChild(tableRow)
+//   })
+//   spinner.hide();
+// })
