72ca50c89a3b4ad54a5c98a32ac11343edd5c69d
Erik <ewpeters@asu.edu>
2025-04-01 22:21:00 -0700

    better quotes

--- chess-server/app/helpers/quotes.rb
+++ chess-server/app/helpers/quotes.rb
@@ -1,14 +1,7 @@
 module Quotes
   QUOTES = [
-    "“Daring ideas are like chessmen moved forward. They may be beaten, but they may start a winning game.\”
+    "\“Daring ideas are like chessmen moved forward. They may be beaten, but they may start a winning game.\”
     ― Johann Wolfgang von Goethe",
-    "\“How dreadful...to be caught up in a game and have no idea of the rules.\”
-    ― Caroline Stevermer, Sorcery & Cecelia: or The Enchanted Chocolate Pot",
-    "\“A queen
-    offers her hand to be kissed,
-    & can form it into a fist
-    while smiling the whole damn time.\”
-    ― Elizabeth Acevedo, Clap When You Land",
     "\“It's an entire world of just 64 squares. I feel safe in it. I can control it; I can dominate it. And it's predictable. So, if I get hurt, I only have myself to blame.\”
     ― Walter Tevis, The Queen's Gambit",
     "\“In chess, as a purely intellectual game, where randomness is excluded, - for someone to play against himself is absurd ...
@@ -16,14 +9,10 @@ module Quotes
     ― Stefan Zweig, Chess Story",
     "\“[Chess] is a foolish expedient for making idle people believe they are doing something very clever, when they are only wasting their time. \”
     ― George Bernard Shaw, The Irrational Knot",
-    "\“I'm a chess piece. A pawn,' she said. 'I can be sacrificed, but I cannot be captured. To be captured would be the end of the game.\”
-    ― Paolo Bacigalupi, Ship Breaker",
     "\“When you see a good move, look for a better one\”
     ― Emanuel Lasker",
     "\“I like the moment when I break a man's ego\”
     ― Bobby Fischer",
-    "\“Living together/ is one move closer/ to living apart\”
-    ― Kristi Maxwell",
     "\“Life is more than just chess.
     Though king dies, life goes on.\”
     ― Toba Beta, Master of Stupidity",
@@ -49,8 +38,6 @@ module Quotes
     ― Walter Tevis, The Queen's Gambit",
     "\“The Pin is mightier than the sword\”
     ― Fred Reinfeld",
-    "\“Beer bottles, whiskey bottles, brown glass, green. They fell to the lawn and I'd feel serene. Adam was king to my stilted queen.\”
-    ― Kate Bernheimer, The Complete Tales of Ketzia Gold",
     "\“Whereas a novice makes moves until he gets checkmated (proof), a Grand Master realizes 20 moves in advance that it’s futile to continue playing (conceptualizing).\”
     ― Bill Gaede",
     "\“What is a weak pawn? A pawn that is exposed to attack and also difficult to defend is a weak pawn. There are several varieties: isolated, doubled, too advanced, retarded.\”
@@ -83,10 +70,6 @@ module Quotes
     ― Robert Hess",
     "\“Behind every move I make on the chess board lies a story of calculation, intuition, and passion. With every game, I discover more about myself and the endless possibilities of the game\”
     ― medicosaurabh",
-    "\“Miss Vesper Holly has the digestive talents of a goat and the mind of a chess master. She is familiar with half a dozen languages and can swear fluently in all of them.\”
-    ― Lloyd Alexander, The Illyrian Adventure",
-    "\“— Vous avez raison, seigneur Virdemis. La politique est une partie d’échecs ; et il se trouve qu’aux échecs, la pièce la plus puissante, c’est la dame.\”
-    ― Olivia Gometz, Les Carmidor : Trahir et Survivre",
     "\“Don't hate the person, hate the action.\”
     ― Mitta Xinindlu",
     "\“The winner of the game is the player who makes the next-to-last mistake.\”
@@ -101,20 +84,11 @@ module Quotes
     ― Vineet Raj Kapoor",
     "\“Checkers is for tramps.\”
     ― Paul Charles Morphy",
-    "\“The Empire State Building is a lady. She’s like the queen in chess, the most powerful piece. She’s like America.\”
-    ― A.D. Aliwat, In Limbo",
-    "\“Truth is simple yet purposely complex.\”
-    ― Wald Wassermann",
     "\“Don't you get bored sometimes?”, she asked.
     “What else is there?”, Benny replied.\”
     ― Walter Tevis, The Queen's Gambit",
-    "\“Chess certainly isn't all there is”, Mrs. Wheatley continued.
-    “It's what I know”, replied Beth.\”
-    ― Walter Tevis, The Queen's Gambit",
     "\“Life is an exchange; you'd think a chess player would know that.\”
     ― Elizabeth Acevedo, Clap When You Land",
-    "\“He was a sweet man who loved words, and played chess with ghosts.\”
-    ― Eley Williams, The Liar's Dictionary",
     "\“There is no moral outcome of a chess match or a poker game as long as skill and stealth rather than cheating have been used.\”
     ― Francis P. Karam, The Truth Engine: Cross-Examination Outside the Box",
     "\“Coaching is more like chess; it’s about out-thinking and outsmarting the other team.\”
@@ -125,8 +99,6 @@ module Quotes
     - Siegbert Tarrasch",
     "\“Can we go to chessy now?\”
     ― Greyson Sweeny",
-    "\“...you could never be completely sure of the other person, so never make a move until you were sure of yourself.\”
-    ― Liz Braswell, Part of Your World",
     "\“You’re just a pawn on the chessboard, Leo Valdez. I was referring to the player who set this ridiculous quest in motion, bringing the Greeks and Romans together.\”
     ― Rick Riordan, The Mark of Athena",
     "\“Excelling at chess has long been considered a symbol of more general intelligence. That is an incorrect assumption in my view, as pleasant as it might be.\”
@@ -141,16 +113,12 @@ module Quotes
     ― Irving Chernev, The Most Instructive Games of Chess Ever Played: 62 Masterpieces of Chess Strategy",
     "\“They say that chess was born in bloodshed.\”
     ― Paolo Maurensig, La variante di Lüneburg",
-    "\“Life is a deadly chess lie.\”
-    ― Deyth Banger",
     "\“Do not expect Ordinary Gameplay on the upside of Life.\”
     ― Vineet Raj Kapoor, UNCHESS: Untie Your Shoes and Walk on the Chessboard of Life",
     "\“The final aim of all of us playing on the board of life is to somehow break out of this board and be free\”
     ― Vineet Raj Kapoor, UNCHESS: Untie Your Shoes and Walk on the Chessboard of Life",
     "\“Gameplay is all our life. Either we guard, attack or develop pieces.\”
     ― Vineet Raj Kapoor, UNCHESS: Untie Your Shoes and Walk on the Chessboard of Life",
-    "\“People are like chess pieces!\”
-    ― Deyth Banger",
     "\“I am not the piece, I am not of the piece, I am not in the piece. I am the move!\”
     ― Niranjan Navalgund",
     "\“The King in chess is indeed a symbol of unity and wholeness and the other pieces are not separate entities but rather parts of “the One Thing”, as Campbell put it.\”
@@ -165,9 +133,6 @@ module Quotes
     ― Ranata Suzuki",
     "\“I used to attack because it was the only thing I knew. Now I attack because I know it works.\”
     ― Garry Kasparov, How Life Imitates Chess: Making the Right Moves, from the Board to the Boardroom",
-    "\“It was going to be our job to annoy someone?”
-    “I know—it’s a dream come true!\”
-    ― Joel N. Ross, The Lost Compass",
     "\“As a rule, the more mistakes there are in a game, the more memorable it remains, because you have suffered and worried over each mistake at the board.\”
     ― Viktor Korchnoi, My Best Games",
     "\“Don't fight over dead Kings and Queens.
@@ -181,9 +146,6 @@ module Quotes
     ― Lisa Renee Jones, Demand",
     "\“Chess problems are the hymn-tunes of mathematics.\”
     ― G H Hardy, A Mathematician's Apology",
-    "\“Uh, she said maybe your eyes matched the Fog like a synchronous magnetic field?”
-    “I don’t even know what language that is.\”
-    ― Joel N. Ross, The Lost Compass",
     "\“If you don’t know how to move, move with the last pawn. The position won't change, but you seem to have done something.\”
     ― Paúliuk Bykoúski",
     "\“The Beauty of the Chess Set is of no import to the Player
@@ -212,37 +174,6 @@ module Quotes
     ― Virginia Alison",
     "\“You have good instincts,trust them. Thinking through every step is fine if you're playing chess, but this isn't chess.\”
     ― Rick Yancey, The 5th Wave",
-    "\“Better I won't answer on this question, I think it's useless so far the future is that it will go in conflict how I know this?
-    It's a tactic taken from chess!\”
-    ― Deyth Banger",
-    "\“If Chess is the switch,” Loretta said, “how does he turn the Fog off?”
-    Bea bit her lower lip. “I don’t know—ask Chess.”
-    “How would I know?” I said. “You try being a switch.\”
-    ― Joel N. Ross, The Lost Compass",
-    "\“Why would they follow me?” I asked Swedish. “It doesn’t make sense.”
-    “They’re made of trash and Fog,” he said. “You think they make sense?\”
-    ― Joel N. Ross, The Lost Compass",
-    "\“Wait a second,” I said. “Did we just win?\”
-    ― Joel N. Ross, The Lost Compass",
-    "\“Everyone have a game, everyone is playing in his own game and his own game he is the king the others are the other figures from the chess.\”
-    ― Deyth Banger",
-    "\“Check your moves well, because it can
-    cost one pawn or losing a lot of just from three moves!\”
-    ― Deyth Banger",
-    "\“You want a fact???
-    ...
-    I'm bad at math but good at chess, I beat the best guy on chess... so you make your own conclusions!\”
-    ― Deyth Banger",
-    "\“We move like the chess game, first the white... then the black... the moves are too fast so far it's difficult to see it. But it's logical, isn't it?\”
-    ― Deyth Banger",
-    "\“In chess puzzles and tactics you see moves which in game is difficult to see mainly because of time and more often from the fast moves...\”
-    ― Deyth Banger",
-    "\“Tactics and Checkmate in 1 move, show me some interesting stuff about chess... so far I can say that I see the chessboard different.\”
-    ― Deyth Banger",
-    "\“Chess Tactics are important in the study.... I don't know about that but I know that this son of biatch beats me on chess games and on chess tactics puzzles he sucks!\”
-    ― Deyth Banger",
-    "\“Life is a mysterious and witty intermingling of fate and events\”
-    ― Alexandra Kosteniuk",
     "\“Of course, errors are not good for a chess game, but errors are unavoidable and in any case, a game without errors, or as they say 'flawless game' is colorless.\”
     ― Mikhail Tal",
     "\“To play for a draw, at any rate with white, is to some degree a crime against chess.\”
@@ -253,8 +184,6 @@ module Quotes
     ― Crystal Woods, Write like no one is reading 2",
     "\“The cherished dream of every chessplayer is to play a match with the World Champion. But here is the paradox: the closer you come to the realization of this goal, the less you think about it.\”
     ― Mikhail Tal",
-    "\“Everyone wants to be wanted and if all people wait for someone else to invest in them, the world will be stuck in an eternal stalemate: nobody moves and nobody wins.\”
-    ― Laura L.",
     "\“Chess is a game with simple rules and pieces, a small sixty-four-space board, but there are more possible chess games than there are atoms in the universe.\”
     ― Austin Grossman, You",
     "\“Zugzwang. It's when you have no good moves. But you still have to move.\”
@@ -278,20 +207,12 @@ module Quotes
     ― Chanda Hahn, Fairest",
     "\“Chess is everything: art, science, and sport.\”
     ― Anatoly Karpov",
-    "\“Go is to Western chess what philosophy is to double-entry accounting.\”
-    ― Trevanian, Shibumi",
     "\“A relationship is a game of chess.\”
     ― Sophie Kinsella, Can You Keep a Secret?",
-    "\“When the end comes, I will meet it raging.\”
-    ― george r.r. martin, Dreamsongs, Volume II",
     "\“Chess is the most intimate game in the world. It's like making love. By the time we finish our first slow game, I will know all his thoughts.\”
     ― Eloisa James, Desperate Duchesses",
     "\“In chess you might find a good move. Then you might find a better move. But take your time. Find the best move.\”
     ― Josh Waitzkin",
-    "\“Life is like playing the game of Chess against yourself, to win you must beat the blacks of you.\”
-    ― Hassaan Ali",
-    "\“Sure. Focused. Let's totally ignore any possible other avenues and just tunnel-vision our way along. Maybe we'll get lucky and blunder into a Lamaru hangout, right?\”
-    ― Stacia Kane, City of Ghosts",
     "\“To many people chess is an extreme sport. It requires a lot of thinking.\”
     ― Ljupka Cvetanova, The New Land",
     "\“The art of chess is akin to the art of war itself; full of strategy and cunning, yet clever placements.\”
@@ -304,13 +225,68 @@ module Quotes
     ― Ljupka Cvetanova, The New Land",
     "\“There are two types of sacrifices: correct ones, and mine.\”
     ― Mikhail Tal",
-    "\“I don't simply create probabilities, I guide them.\”
-    ― Lionel Suggs",
     "\“I'm playing chess while everyone else is playing checkers, that's why I always lose\”
     ― Josh Stern",
-    "\“Some guys live in worlds where pawns stay pawns. I'm one move from king.\”
-    ― Darnell Lamont Walker",
     "\“I want to be an aggressive player;One who always find joy in every move.\”
-    ― Kenneth de Guzman"
+    ― Kenneth de Guzman",
+    "\“The laws of Chess do not permit a free choice: you have to move whether you like it or not.\”
+    ― Emanuel Lasker",
+    "\“I look one move ahead… the best!\”
+    ― Siegbert Tarrasch",
+    "\“First-class players lose to second-class players because second-class players sometimes play a first-class game.\”
+    ― Siegbert Tarrasch",
+    "\“Weak points or holes in the opponent’s position must be occupied by pieces not Pawns.\”
+    ― Siegbert Tarrasch",
+    "\“In Chess, as it is played by masters, chance is practically eliminated.\”
+    ― Emanuel Lasker",
+    "\“The passion for playing Chess is one of the most unaccountable in the world.\”
+    ― H.G. Wells",
+    "\“The older I grow, the more I value Pawns.\”
+    ― Keres",
+    "\“The sign of a great Master is his ability to win a won game quickly and painlessly.\”
+    ― Irving Chernev",
+    "\“Those who say they understand Chess, understand nothing.\”
+    ― Robert Hubner",
+    "\“One bad move nullifies forty good ones.\”
+    ― Horowitz",
+    "\“Every Pawn is a potential Queen.\”
+    ― James Mason",
+    "\“I prefer to lose a really good game than to win a bad one.\”
+    ― David Levy",
+    "\“Chess is a terrific way for kids to build self image and self esteem.\”
+    ― Saudin Robovic",
+    "\“When you are lonely, when you feel yourself an alien in the world, play Chess. This will raise your spirits and be your counselor in war.\”
+    ― Aristotle",
+    "\“Pawns: they are the soul of this game, they alone form the attack and defense.\”
+    ― Philidor",
+    "\“Chess is 99 percent tactics.\”
+    ― Teichmann",
+    "\“Chess is war over the board. The object is to crush the opponents mind.\”
+    ― Bobby Fischer",
+    "\“Chess demands total concentration.\”
+    ― Bobby Fischer",
+    "\“Chess is the art which expresses the science of logic\”
+    ― Mikhail Botvinnik",
+    "\“A bad plan is better than none at all.\”
+    ― Frank Marshall",
+    "\“Chess is life.\”
+    ― Bobby Fischer",
+    "\“Chess is mental torture.\”
+    ― Garry Kasparov",
+    "\“The mistakes are there, waiting to be made.\”
+    ― Savielly Tartakower",
+    "\“Tactics flow from a superior position.\”
+    ― Bobby Fischer",
+    "\“A strong memory, concentration, imagination, and a strong will is required to become a great chess player.\”
+    ― Bobby Fischer",
+    "\“The task of the positional player is systematically to accumulate slight advantages and try to convert temporary advantages into permanent ones, otherwise the player with the better position runs the risk of losing it.\”
+    ― Wilhelm Steinitz",
+    "\“Without error there can be no brilliancy.\”
+    ― Emanuel Lasker",
+    "\“No one ever won a game by resigning.\”
+    ― Savielly Tartakower",
+    "\“Self-confidence is very important. If you don’t think you can win, you will take cowardly decisions in the crucial moments, out of sheer respect for your opponent.\”
+    ― Magnus Carlsen"
+
   ]
 end
